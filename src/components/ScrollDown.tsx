a48da7cc25876bc7fab4a77027b6d53d76d5add6
kevin <kevinfng12@gmail.com>
2025-05-11 23:41:25 -0400

    update 'scroll down' component to toggle bounce animation for whole component. update landing page height to be calculated based on screen height and navbar height

--- src/components/ScrollDown.tsx
+++ src/components/ScrollDown.tsx
@@ -2,10 +2,10 @@ import { BsChevronCompactDown } from 'react-icons/bs';
 
 const ScrollDown = () => {
   return (
-    <a className='flex justify-center w-full absolute bottom-6' href='#scroll'>
+    <a className='flex group justify-center w-full absolute bottom-6' href='#scroll'>
       <div>
         <h6 className='text-white/40'>Scroll Down</h6>
-        <BsChevronCompactDown className='w-full h-auto text-white/40 px-4 -mt-4 hover:animate-bounce cursor-pointer' />
+        <BsChevronCompactDown className='w-full h-auto text-white/40 px-4 -mt-4 group-hover:animate-bounce cursor-pointer' />
       </div>
     </a>
   );
