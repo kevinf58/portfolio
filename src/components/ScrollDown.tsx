5deaa6d362070976b2bd37a3a10e8ce70b0f8502
kevin <kevinfng12@gmail.com>
2025-05-11 22:47:39 -0400

    remove unecessary flexboxs

--- src/components/ScrollDown.tsx
+++ src/components/ScrollDown.tsx
@@ -3,7 +3,7 @@ import { BsChevronCompactDown } from 'react-icons/bs';
 const ScrollDown = () => {
   return (
     <a className='flex justify-center w-full absolute bottom-6' href='#scroll'>
-      <div className='flex flex-col'>
+      <div>
         <h6 className='text-white/40'>Scroll Down</h6>
         <BsChevronCompactDown className='w-full h-auto text-white/40 px-4 -mt-4 hover:animate-bounce cursor-pointer' />
       </div>
