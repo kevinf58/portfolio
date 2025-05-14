26b9f41b10afc85b0bafd04fbc5d9637a2032162
kevin <kevinfng12@gmail.com>
2025-05-14 11:02:44 -0400

    fix inconsistent icon sizes

--- src/components/common/AwardSection.tsx
+++ src/components/common/AwardSection.tsx
@@ -3,25 +3,25 @@ import { BiAward } from 'react-icons/bi';
 
 const AwardSection = () => {
   return (
-    <section className='w-full bg-black px-32 py-24' id='scroll'>
+    <section className='w-full bg-black sm:px-32 px-6 py-24' id='scroll'>
       <h1 className='text-3xl font-medium lg:text-start text-center'>Awards</h1>
-      <div className='flex flex-wrap justify-center mx-10 my-12 gap-6'>
+      <div className='flex flex-wrap justify-center gap-10 pt-10 pb-8'>
         <Box>
           <div className='flex'>
-            <BiAward className='w-20 h-auto mr-6 my-auto text-primary' />
-            <div className='flex flex-col'>
-              <span className='text-sm font-semibold'>2024 TD Scholarship in Computer Science</span>
+            <BiAward size={60} className='my-auto mr-6 text-primary' />
+            <div className='flex flex-col w-48'>
+              <span className='text-sm font-semibold'>2025 TD Scholarship in Computer Science</span>
               <span className='text-xs text-white/50 my-1 break-words'>
                 Awarded for Academic Excellence
               </span>
-              <h6 className='text-sm text-white/50 mt-2'>$1500</h6>
+              <h6 className='text-sm text-white/50 mt-2'>$600</h6>
             </div>
           </div>
         </Box>
         <Box>
           <div className='flex'>
-            <BiAward className='w-20 h-auto mr-6 my-auto text-primary' />
-            <div className='flex flex-col'>
+            <BiAward size={60} className='my-auto mr-6 text-primary' />
+            <div className='flex flex-col w-48'>
               <span className='text-sm font-semibold'>2024 Toronto Tech Expo Winner</span>
               <span className='text-xs text-white/50 my-1 break-words'>
                 Awarded for Academic Excellence
@@ -32,18 +32,18 @@ const AwardSection = () => {
         </Box>
         <Box>
           <div className='flex'>
-            <BiAward className=' w-20 h-auto mr-6 my-auto text-primary' />
-            <div className='flex flex-col'>
-              <span className='text-sm font-semibold'>2023 TD Scholarship in Computer Science</span>
+            <BiAward size={60} className='my-auto mr-6 text-primary' />
+            <div className='flex flex-col w-48'>
+              <span className='text-sm font-semibold'>2025 TD Scholarship in Computer Science</span>
               <span className='text-xs text-white/50 my-1 break-words'>
                 Awarded for Academic Excellence
               </span>
-              <h6 className='text-sm text-white/50 mt-2'>$1500</h6>
+              <h6 className='text-sm text-white/50 mt-2'>$600</h6>
             </div>
           </div>
         </Box>
       </div>
-      <div className='h-0.5 bg-white/40 rounded-full sm:mx-10' />
+      <div className='h-0.5 bg-white/40 rounded-full sm:mx-20' />
     </section>
   );
 };
--- src/components/common/Box.tsx
+++ src/components/common/Box.tsx
@@ -2,7 +2,7 @@ import { BoxProps } from '@/types/BoxProps';
 
 const Box = (props: BoxProps) => {
   return (
-    <div className='w-88 min-w-[19.25rem] px-6 py-4 bg-white/8 rounded-xl shadow-primary'>
+    <div className='w-72 px-6 py-4 bg-white/8 rounded-xl shadow-primary'>
       {props.children}
     </div>
   );
