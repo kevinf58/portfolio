c74f9442123b486360bdf76855f6c6e8cceb9f32
kevin <kevinfng12@gmail.com>
2025-05-21 00:40:20 -0400

    tweak backdrop dropdown speed

--- src/components/common/NavBarHamburger.tsx
+++ src/components/common/NavBarHamburger.tsx
@@ -51,7 +51,7 @@ const NavBarHamburger = () => {
         </div>
       </div>
       <div
-        className={`fixed top-0 left-0 w-screen h-screen bg-black/30 transform transition-transform duration-300 ease-in z-10 ${
+        className={`fixed top-0 left-0 w-screen h-screen bg-black/30 transform transition-transform duration-200 ease-in z-10 ${
           open ? 'translate-y-0 shadow-primary' : '-translate-y-full'
         }`}
         onClick={toggleMenu}
