2b74f214f3244909e10404675c907af61b416e69
kevin <kevinfng12@gmail.com>
2025-05-22 23:34:46 -0400

    css tweaks

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -8,67 +8,67 @@ const Technologies = () => {
     <>
       <h1 className='text-3xl font-medium lg:text-start text-center'>Technologies</h1>
       <div className='flex flex-wrap gap-4 mt-8 mx-6'>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaReact className='text-[#30A2FF]' />
             React
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <SiTypescript className='text-[#3178C6] bg-white rounded-xs' />
             TypeScript
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <SiJavascript className='text-[#FFDF00] bg-black' />
             TypeScript
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaPython className='text-[#336D9C]' />
             Python
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaHtml5 className='text-[#FC490B]' />
             HTML
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaCss3Alt className='text-[#2196F3]' />
             CSS
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <SiTailwindcss className='text-[#38BDF9]' />
             Tailwind CSS
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaDatabase className='text-white' />
             SQL
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <RiNextjsFill className='text-[#161416]' />
             Next.JS
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaGitAlt className='text-[#F2645A]' />
             Git
           </>
         </Card>
-        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1'>
+        <Card className='w-fit !px-4.5 !py-1.5 flex items-center text-sm font-medium gap-1.5'>
           <>
             <FaJava className='text-[#DE8E2F]' />
             Java
--- src/components/common/Cursor.tsx
+++ src/components/common/Cursor.tsx
@@ -31,11 +31,11 @@ const Cursor = () => {
 
   const conditionalSizing = hoveringClickable
     ? mouseDown
-      ? 'w-10 h-10'
-      : 'w-12 h-12'
+      ? 'w-9 h-9 duration-0'
+      : 'w-12 h-12 duration-200'
     : mouseDown
-    ? 'w-2.5 h-2.5'
-    : 'w-3 h-3';
+    ? 'w-2.5 h-2.5 duration-0'
+    : 'w-3 h-3 duration-200';
 
   const conditionalBg = hoveringClickable ? 'bg-black/55' : 'bg-primary/65';
 
@@ -45,7 +45,7 @@ const Cursor = () => {
       style={{ left: pos.x, top: pos.y, transform: 'translate(-50%, -50%)' }}
     >
       <div
-        className={`rounded-full transition-all duration-150 ${conditionalSizing} ${conditionalBg}`}
+        className={`rounded-full transition-all ${conditionalSizing} ${conditionalBg}`}
       />
     </div>
   );
