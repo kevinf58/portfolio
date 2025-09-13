f15b035f0404ffc7050994e2b6b2d02dae0c9210
Kevin <kevinfng12@gmail.com>
2025-09-12 22:57:48 -0400

    add small refactoring changes

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -1,4 +1,4 @@
-import { DividerProps } from "@/types/DividerProps";
+import { DividerProps } from "@/types/components/Divider.type";
 
 const Divider = (props: DividerProps) => {
   return <div className={`max-w-7xl h-0.5 bg-white/40 rounded-full sm:mx-20 mt-8 mb-16 ${props.className}`} />;
