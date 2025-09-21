088fcc51c2a886fd4a13c202dbcc96c67f99652e
Kevin <kevinfng12@gmail.com>
2025-09-21 11:10:47 -0400

    rearrange button placement in html tre

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -15,12 +15,12 @@ const Page = () => {
         </div>
         <div className="w-1/2 h-full flex flex-col border-l-2 border-tint/20">
           <h1 className="bg-primary/10 font-bold py-3 px-6 border-b-2 border-tint/20 shadow-primary">Markdown</h1>
-          <div className="w-full flex-1 py-1 px-2 font-serif whitespace-pre-wrap">
+          <div className="w-full flex-1 py-1 px-2 font-serif whitespace-pre-wrap leading-4 overflow-y-auto">
             {markdown}
-            <Button className="!fixed bottom-5 right-10" type="hollow" onClick={() => console.log(markdown)}>
-              Submit
-            </Button>
           </div>
+          <Button className="!fixed bottom-5 right-10" type="hollow" onClick={() => console.log(markdown)}>
+            Submit
+          </Button>
         </div>
       </div>
     </section>
