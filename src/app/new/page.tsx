20cc0bd8d5d1035a43082882111299eb986bc92a
Kevin <kevinfng12@gmail.com>
2025-09-20 23:34:26 -0400

    add base crepe editor + markdown representation

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,11 +1,26 @@
-import Editor from "@/components/common/Editor";
+"use client";
+
+import { Button } from "@/components/common/Button";
+import Editor from "@/components/Editor";
+import { useState } from "react";
 
 const Page = () => {
+  const [markdown, setMarkdown] = useState("");
+
   return (
     <section className="relative h-[calc(100vh-4.75rem)] w-full flex flex-col justify-center font-medium bg-light-black shadow-primary">
-      <div className="h-full w-full">
+      <div className="h-full w-full flex bg-white/3">
         <div className="bg-black h-full w-1/2 overflow-y-scroll">
-          <Editor />
+          <Editor setMarkdown={setMarkdown} />
+        </div>
+        <div className="w-1/2 h-full flex flex-col border-l-2 border-tint/20">
+          <h1 className="bg-primary/10 font-bold py-3 px-6 border-b-2 border-tint/20 shadow-primary">Markdown</h1>
+          <div className="w-full flex-1 py-1 px-2 font-serif whitespace-pre-wrap">
+            {markdown}
+            <Button className="!fixed bottom-5 right-10" type="hollow" onClick={() => console.log(markdown)}>
+              Submit
+            </Button>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/Editor.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Crepe } from "@milkdown/crepe";
+import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
+import { EditorProps } from "@/types/components/Editor.type";
+import "@milkdown/crepe/theme/common/style.css";
+
+const CrepeEditor = (props: EditorProps) => {
+  useEditor((root) => {
+    const crepe = new Crepe({ root });
+
+    // listen for updates in crepe to invoke setMarkdown and send it back up the tree
+    crepe.on((listener) => {
+      listener.updated(() => {
+        if (crepe) {
+          const markdown = crepe.getMarkdown();
+
+          props.setMarkdown(markdown);
+          console.log(markdown);
+        }
+      });
+    });
+
+    return crepe;
+  }, []);
+
+  return <Milkdown />;
+};
+
+const Editor = (props: EditorProps) => {
+  return (
+    <MilkdownProvider>
+      <CrepeEditor setMarkdown={props.setMarkdown} />
+    </MilkdownProvider>
+  );
+};
+
+export default Editor;
