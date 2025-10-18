a3c7ef46f721eebe9e61b5323f3653a41af27afe
Kevin <kevinfng12@gmail.com>
2025-10-18 18:00:54 -0400

    add image support for journals/projects

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,6 +4,8 @@ import { Crepe } from "@milkdown/crepe";
 import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
 import { EditorProps } from "@/types/components/Editor.type";
 import "@milkdown/crepe/theme/common/style.css";
+import uploadHandler from "@/utils/uploadHandler";
+import { imageBlockConfig } from "@milkdown/kit/component/image-block";
 
 const CrepeEditor = (props: EditorProps) => {
   useEditor((root) => {
@@ -17,6 +19,23 @@ const CrepeEditor = (props: EditorProps) => {
 
           props.setMarkdown(markdown);
         }
+      });
+    });
+
+    crepe.on((listener) => {
+      listener.mounted(() => {
+        crepe.editor.ctx.update(imageBlockConfig.key, (defaultConfig) => ({
+          ...defaultConfig,
+          onUpload: async (file: File) => {
+            const url = await uploadHandler(file);
+            return url;
+          },
+        }));
+      });
+
+      listener.updated(() => {
+        const markdown = crepe.getMarkdown();
+        props.setMarkdown(markdown);
       });
     });
 
---
+++ src/types/api/Project.type.ts
@@ -0,0 +1,15 @@
+export type JournalType = {
+  id: number;
+  title: string;
+  date: Date;
+  tags: string[];
+  markdown: string;
+};
+
+export type RawJournalType = Omit<JournalType, "id">;
+
+export type JournalPageParams = {
+  params: {
+    id: string;
+  };
+};
