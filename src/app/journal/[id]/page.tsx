b6bf7472911fedca940053deca53c42b51be8704
Kevin <kevinfng12@gmail.com>
2025-10-18 20:34:52 -0400

    remove hover:scale-101 to avoid the decreasing of image quality

--- src/app/journal/[id]/page.tsx
+++ src/app/journal/[id]/page.tsx
@@ -22,7 +22,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <section className="relative flex justify-center min-h-[calc(100vh-4.75rem)] w-full bg-light-black shadow-default py-36 md:px-10 sm:px-6 px-2">
-      <div className="min-h-full max-w-[65rem] w-full lg:px-48 sm:px-20 px-8 lg:py-32 sm:py-24 py-12 gap-1 rounded-sm border-2 border-tint/10 bg-gray hover:scale-101 duration-100">
+      <div className="min-h-full max-w-[65rem] w-full lg:px-48 sm:px-20 px-8 lg:py-32 sm:py-24 py-12 gap-1 rounded-sm border-2 border-tint/10 bg-gray">
         <ReadOnlyCrepe markdown={journal.markdown} />
       </div>
       <DeleteButton id={Number(id)} />
